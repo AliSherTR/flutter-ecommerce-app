3e9f972a3c814e6218fc0dc076f1d33f993cbdf3
AliSherTR <alisherkhan817@gmail.com>
2025-05-06 15:16:14 +0500

    split form into multiple widgets

--- lib/features/authentication/screens/signup/sign_up.dart
+++ lib/features/authentication/screens/signup/sign_up.dart
@@ -28,66 +28,7 @@ class SignUpScreen extends StatelessWidget {
               ),
               const SizedBox(height: TSizes.spaceBtwSections),
               // form
-              Form(
-                child: Column(
-                  children: [
-                    Row(
-                      children: [
-                        Expanded(
-                          child: TextFormField(
-                            decoration: const InputDecoration(
-                              labelText: TTexts.firstName,
-                              prefixIcon: Icon(Iconsax.user),
-                            ),
-                          ),
-                        ),
-                        const SizedBox(width: TSizes.spaceBtwInputFields),
-                        Expanded(
-                          child: TextFormField(
-                            decoration: const InputDecoration(
-                              labelText: TTexts.lastName,
-                              prefixIcon: Icon(Iconsax.user),
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                    // user name
-                    const SizedBox(height: TSizes.spaceBtwInputFields),
-                    TextFormField(
-                      decoration: const InputDecoration(
-                        labelText: TTexts.username,
-                        prefixIcon: Icon(Iconsax.user_edit),
-                      ),
-                    ),
-                    const SizedBox(height: TSizes.spaceBtwInputFields),
-                    // email
-                    TextFormField(
-                      decoration: const InputDecoration(
-                        labelText: TTexts.email,
-                        prefixIcon: Icon(Iconsax.direct),
-                      ),
-                    ),
-                    const SizedBox(height: TSizes.spaceBtwInputFields),
-                    // phone number
-                    TextFormField(
-                      decoration: const InputDecoration(
-                        labelText: TTexts.phoneNo,
-                        prefixIcon: Icon(Iconsax.call),
-                      ),
-                    ),
-                    const SizedBox(height: TSizes.spaceBtwInputFields),
-                    // password
-                    TextFormField(
-                      decoration: const InputDecoration(
-                        labelText: TTexts.password,
-                        prefixIcon: Icon(Iconsax.lock),
-                        suffixIcon: Icon(Iconsax.eye_slash),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
+              Sign_up_form(),
               const SizedBox(height: TSizes.spaceBtwSections),
               Row(
                 children: [
@@ -148,6 +89,76 @@ class SignUpScreen extends StatelessWidget {
             ],
           ),
         ),
+      ),
+    );
+  }
+}
+
+class Sign_up_form extends StatelessWidget {
+  const Sign_up_form({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      child: Column(
+        children: [
+          Row(
+            children: [
+              Expanded(
+                child: TextFormField(
+                  decoration: const InputDecoration(
+                    labelText: TTexts.firstName,
+                    prefixIcon: Icon(Iconsax.user),
+                  ),
+                ),
+              ),
+              const SizedBox(width: TSizes.spaceBtwInputFields),
+              Expanded(
+                child: TextFormField(
+                  decoration: const InputDecoration(
+                    labelText: TTexts.lastName,
+                    prefixIcon: Icon(Iconsax.user),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          // user name
+          const SizedBox(height: TSizes.spaceBtwInputFields),
+          TextFormField(
+            decoration: const InputDecoration(
+              labelText: TTexts.username,
+              prefixIcon: Icon(Iconsax.user_edit),
+            ),
+          ),
+          const SizedBox(height: TSizes.spaceBtwInputFields),
+          // email
+          TextFormField(
+            decoration: const InputDecoration(
+              labelText: TTexts.email,
+              prefixIcon: Icon(Iconsax.direct),
+            ),
+          ),
+          const SizedBox(height: TSizes.spaceBtwInputFields),
+          // phone number
+          TextFormField(
+            decoration: const InputDecoration(
+              labelText: TTexts.phoneNo,
+              prefixIcon: Icon(Iconsax.call),
+            ),
+          ),
+          const SizedBox(height: TSizes.spaceBtwInputFields),
+          // password
+          TextFormField(
+            decoration: const InputDecoration(
+              labelText: TTexts.password,
+              prefixIcon: Icon(Iconsax.lock),
+              suffixIcon: Icon(Iconsax.eye_slash),
+            ),
+          ),
+        ],
       ),
     );
   }
